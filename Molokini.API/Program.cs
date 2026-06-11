f7f1085f5fb99ec1632097308b56ed188e42edf7
Craig Whitehouse <c.whitehouse@live.co.uk>
2026-06-11 11:11:22 +0100

    Updated project to include Blazor frontend, using MadBlazor

--- Molokini.API/Program.cs
+++ Molokini.API/Program.cs
@@ -1,18 +1,40 @@
 using Molokini.API.Endpoints;
+using Molokini.API.Middleware;
 using Molokini.Core.Interfaces;
 using Molokini.Core.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Register Exception Handling & ProblemDetails formatting
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins("https://localhost:7003") // Blazor port
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
+
 // Map services
 builder.Services.AddScoped<IUserService, UserService>();
 
+// Add Blazor frontend
+builder.Services.AddScoped(client => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7296") // Web API port
+});
+
 var app = builder.Build();
+
+app.UseExceptionHandler();
+app.UseCors();
 
 // Map endpoints
 app.MapUserEndpoints();
